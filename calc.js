64ecea9094aa0fc9febf9d9017a1fd8f19acf5f4
Gino Tasis <gtasis84@gmail.com>
2021-12-14 16:14:56 -0500

    Calculator fully functioning

--- calc.js
+++ calc.js
@@ -15,7 +15,23 @@ reader.question("What would you like to calculate?", function(input){
 	num2 = Number(tokens[2]);
 
 
+	if (mathSymbol == "+") {
+		console.log(num1 + num2)
+	} else if (mathSymbol == "-") {
+		console.log(num1 - num2)
+	} else if (mathSymbol == "/") {
+		console.log(num1 / num2)
+	} else if (mathSymbol == "*") {
+		console.log(num1 * num2)
+	} else if (mathSymbol == "^2") {
+		console.log(Math.sqrt(num1))
+	}  
+	else {
+		console.log("Operator is invalid")
+	}
+
 	// This line closes the connection to the command line interface.
 	reader.close()
 
 });
+
